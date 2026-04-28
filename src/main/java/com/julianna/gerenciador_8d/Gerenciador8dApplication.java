19a73bfd6bb5cfb8d9f158a48412a1c92c628da2
Julianna Fernandes <juliannabf29@gmail.com>
2026-04-27 22:15:51 -0300

    chore: commit inicial

---
+++ src/main/java/com/julianna/gerenciador_8d/Gerenciador8dApplication.java
@@ -0,0 +1,13 @@
+package com.julianna.gerenciador_8d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gerenciador8dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gerenciador8dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julianna/gerenciador_8d/Gerenciador8dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianna.gerenciador_8d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gerenciador8dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
