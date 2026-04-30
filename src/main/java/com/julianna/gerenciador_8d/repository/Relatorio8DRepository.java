01a558617091a53b94452c3ebc7f677a5adced17
Julianna Fernandes <juliannabf29@gmail.com>
2026-04-29 21:55:02 -0300

    feat: criando repository Relatorio8D

---
+++ src/main/java/com/julianna/gerenciador_8d/repository/Relatorio8DRepository.java
@@ -0,0 +1,17 @@
+package com.julianna.gerenciador_8d.repository;
+
+import com.julianna.gerenciador_8d.entity.Relatorio8D;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+@Repository
+public interface Relatorio8DRepository  extends JpaRepository<Relatorio8D, Long> {
+    List<Relatorio8D> findByTituloContainingIgnoreCase(String titulo);
+    List<Relatorio8D> findByTituloContainingIgnoreCase(String titulo, Pageable peageable);
+    List<Relatorio8D> findByStatus(Relatorio8D.Status8D status);
+    long countByStatus(Relatorio8D.Status8D status);
+
+}
